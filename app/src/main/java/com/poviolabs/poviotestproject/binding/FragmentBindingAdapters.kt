3493a3da57fe94bb63064f5f95273edda996d260
aleksamarkoni <aleksamarkon@gmail.com>
2018-06-04 03:29:31 +0200

    Minor code changes to support plurals

--- app/src/main/java/com/poviolabs/poviotestproject/binding/FragmentBindingAdapters.kt
+++ app/src/main/java/com/poviolabs/poviotestproject/binding/FragmentBindingAdapters.kt
@@ -3,7 +3,9 @@ package com.poviolabs.poviotestproject.binding
 import android.databinding.BindingAdapter
 import android.support.v4.app.Fragment
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.poviolabs.poviotestproject.R
 import javax.inject.Inject
 
 /**
@@ -12,6 +14,14 @@ import javax.inject.Inject
 class FragmentBindingAdapters @Inject constructor(val fragment: Fragment) {
     @BindingAdapter("imageUrl")
     fun bindImage(imageView: ImageView, url: String?) {
-        Glide.with(fragment).load("http:" + url).into(imageView)
+        Glide.with(fragment).load(url).into(imageView)
+    }
+
+    @BindingAdapter("sightings")
+    fun bindSightings(textView: TextView, sightings: Int) {
+        when (sightings) {
+            0 -> textView.text = fragment.resources.getString(R.string.no_sightings)
+            else -> textView.text = fragment.resources.getQuantityString(R.plurals.sightings, sightings, sightings)
+        }
     }
 }
